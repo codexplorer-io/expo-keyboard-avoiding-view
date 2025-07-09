063c2f62b1bc40402edbe1fcf226fb8b7194f663
Marko Matic <mail.marko.matic@gmail.com>
2025-07-09 20:25:08 +1000

    - Update keyboard avoiding view to support android edge to edge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import {
     KeyboardAvoidingView as RNKeyboardAvoidingView,
-    Platform
-} from 'react-native';
+    KeyboardAwareScrollView,
+    KeyboardStickyView
+} from 'react-native-keyboard-controller';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export const KeyboardAvoidingView = props => (
-    <RNKeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        {...props}
-    />
-);
+export const KeyboardAvoidingView = ({
+    hasScroll,
+    isSticky,
+    children,
+    ...props
+}) => {
+    const insets = useSafeAreaInsets();
+    let View = RNKeyboardAvoidingView;
+
+    if (hasScroll) {
+        View = KeyboardAwareScrollView;
+    }
+
+    if (isSticky) {
+        View = KeyboardStickyView;
+    }
+
+    return (
+        <View
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            extraKeyboardSpace={-insets.bottom}
+            {...props}
+        >
+            {children}
+        </View>
+    );
+};
