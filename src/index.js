106df03e7a57a69da3fad6a536ac4ceb3f0c46f5
Marko Matic <mail.marko.matic@gmail.com>
2026-08-07 14:37:35 +1000

    - Fix keyboard in modal

--- src/index.js
+++ src/index.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import { Platform } from 'react-native';
 import {
     KeyboardAvoidingView as RNKeyboardAvoidingView,
-    KeyboardAwareScrollView,
-    KeyboardStickyView
+    Platform
+} from 'react-native';
+import {
+    KeyboardAvoidingView as RNCKeyboardAvoidingView,
+    KeyboardAwareScrollView as RNCKeyboardAwareScrollView,
+    KeyboardStickyView as RNCKeyboardStickyView
 } from 'react-native-keyboard-controller';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const KeyboardAvoidingView = ({
-    hasScroll,
-    isSticky,
+    hasScroll = false,
+    isSticky = false,
+    isInModal = false,
     children,
     ...props
 }) => {
     const insets = useSafeAreaInsets();
-    let View = RNKeyboardAvoidingView;
+
+    if (isInModal) {
+        return (
+            <RNKeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                {...props}
+            >
+                {children}
+            </RNKeyboardAvoidingView>
+        );
+    }
+
+    let View = RNCKeyboardAvoidingView;
 
     if (hasScroll) {
-        View = KeyboardAwareScrollView;
+        View = RNCKeyboardAwareScrollView;
     }
 
     if (isSticky) {
-        View = KeyboardStickyView;
+        View = RNCKeyboardStickyView;
     }
 
     return (
